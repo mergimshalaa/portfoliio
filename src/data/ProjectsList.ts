d36a0e558e093579f9f0df7f170ab5681e0f6e7d
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-02 19:40:38 +0100

    data projects list (work in progress)

---
+++ src/data/ProjectsList.ts
@@ -0,0 +1,83 @@
+interface ProjectsCards {
+  title: string;
+  description: string;
+  link: string;
+}
+
+export const ProjectsData: ProjectsCards[] = [
+  {
+    title: "Weather App",
+    description:
+      "This was my first project working with API, I created a weather app displaying current weather with the date.",
+    link: "https://mergimshalaa.github.io/weatherApp/",
+  },
+  {
+    title: "Palimeda Shoes",
+    description:
+      "School project that was created by a group of four people using React, React Bootstrap and TypeScript to build an e-commerce platform.",
+    link: "https://github.com/plugga-tech/react-webshop-ts-parham-mergim-daniel-linus",
+  },
+  {
+    title: "Save Gothenburg",
+    description:
+      "School project where we as a group of six persons created a game with OOP and with library p5.js and Typescript.",
+    link: "https://save-gothenburg.netlify.app/",
+  },
+  {
+    title: "STORM by PAMEDA",
+    description:
+      "Miniproject, we created a web application working with API and functionality. This was created with React and Typescript.",
+    link: "https://stormweatherappbypameda.netlify.app/",
+  },
+  {
+    title: "Todo List",
+    description:
+      "Created a todo list to manage and practice Vanilla Javascript. This project was created with HTML & CSS and Javascript.",
+    link: "https://mergimshalaa.github.io/Todo-list/",
+  },
+  {
+    title: "Resume",
+    description:
+      "Schoolproject were the task was to create an interactive & responsive resume of myself. Created with HTML & CSS and Vanilla Javascript.",
+    link: "https://mergimshalaa.github.io/Lab-cv/",
+  },
+  {
+    title: "Codify - Still in process",
+    description:
+      "Created a startup for a website and it is still in process. This project is created with HTML, CSS and Vanilla Javascript.",
+    link: "https://mergimshalaa.github.io/Codify-website/",
+  },
+  {
+    title: "Ölstugan Website copy",
+    description:
+      "School project with two classmates, the task was to modernize a choosen website. This project was created with HTML & CSS.",
+    link: "https://mergimshalaa.github.io/Olstugan-copy/",
+  },
+  {
+    title: "Apple Website copy",
+    description:
+      "First school project were me and another classmate created this apple website copy. Created with HTML & CSS.",
+    link: "https://mergimshalaa.github.io/Apple-Hemsida/",
+  },
+  {
+    title: "Login Form Tesla copy",
+    description:
+      "School exercise were the task was to build Tesla login form with local storage. This project was created with HTML & CSS.",
+    link: "https://mergimshalaa.github.io/ovning-form/",
+  },
+  {
+    title: "",
+    description:"",
+    link: "",
+  },
+  {
+    title: "",
+    description:"",
+    link: "",
+  },
+  {
+    title: "",
+    description:"",
+    link: "",
+  },
+];
