b9fb651a35ca3d8d3fa86a2ff1c584546eaa66d9
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-03 21:40:39 +0100

    add routes in application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { Home } from "./pages/HomePage/HomePage"
 import "./App.css"
+import { Container } from "react-bootstrap"
+import { Route, Routes } from "react-router-dom"
+import { AboutPage } from "./pages/About/AboutPage"
+import { ProjectsPage } from "./pages/Projects/ProjectsPage"
+import { ResumePage } from "./pages/Resume/ResumePage"
 
 function App() {
   return (
     <>
-    <Home />
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/resume" element={<ResumePage />} />
+            </Routes>
+          </Container>
     </>
   )
 }
