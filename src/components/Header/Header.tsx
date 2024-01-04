f0b0b7dd53967e8a0e69f82fa20b34c67c3493ad
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-04 14:24:36 +0100

    fix header, navigation works

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, NavbarBrand, Navbar as NavbarBs } from "react-bootstrap";
+import { FaHome } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { StyledLink, StyledLinkHome, StyledNav } from "./header-styled-components";
+import { HamburgerMenu } from "../MenuToggle/HamburgerMenu";
+
 export function Header() {
-    return (
-      <>
-       <div>my navbar</div>
-      </>
-    );
-  }
+  return (
+    <NavbarBs className="bg-light header sticky-top">
+      <div>
+        <HamburgerMenu />
+      </div>
+      <StyledLinkHome to="/" as={NavLink}>
+        <FaHome style={{ fontSize: "2rem" }} />
+      </StyledLinkHome>
+      <Container
+        className="d-flex justify-content-end align-items-center flex-grow-1"
+        style={{ paddingRight: "55px" }}
+      >
+        <StyledNav>
+          <StyledLink to="/about" as={NavLink}>
+            ABOUT
+          </StyledLink>
+          <StyledLink to="/projects" as={NavLink}>
+            PROJECTS
+          </StyledLink>
+          <StyledLink to="/resume" as={NavLink}>
+            RESUME
+          </StyledLink>
+        </StyledNav>
+      </Container>
+    </NavbarBs>
+  );
+}
