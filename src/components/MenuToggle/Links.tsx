d3f59d7da4c65787d919434583dd2a53441b7e15
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-05 01:17:17 +0100

    styling navlinks in smallerdevices

---
+++ src/components/MenuToggle/Links.tsx
@@ -0,0 +1,52 @@
+import { Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { FaHome, FaUser, FaProjectDiagram, FaFileAlt } from "react-icons/fa";
+
+  interface NavbarLinksProps {
+    handleClose: () => void;
+  }
+  
+  export function NavbarLinks({ handleClose }: NavbarLinksProps)  {
+    return (
+      <Nav className="flex-column">
+        <StyledLink onClick={handleClose} to="/" exact as={NavLink}>
+          <FaHome style={{ marginRight: "5px" }} /> Home
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/about" as={NavLink} >
+          <FaUser style={{ marginRight: "5px" }} /> About
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/projects" as={NavLink} >
+          <FaProjectDiagram style={{ marginRight: "5px" }} /> Projects
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/resume" as={NavLink}>
+          <FaFileAlt style={{ marginRight: "5px" }} /> Resume
+        </StyledLink>
+      </Nav>
+    );
+  }
+  
+
+const StyledLink = styled(NavLink)<{ exact?: boolean }>`
+  color: white;
+  margin-right: 10px;
+  margin-bottom: 30px;
+  letter-spacing: 1px;
+  font-weight: 300;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #B3B3B3;
+
+  text-decoration: none;
+  font-size: 1.3rem;
+  transition: all 0.7s ease;
+  outline: none;
+  position: relative;
+  
+
+  &:hover {
+    color: blue;
+  }
+`;
