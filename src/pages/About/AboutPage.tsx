f40212ec08d7a36b4a1ae0798041df9b1c50569f
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-07 18:56:26 +0100

    map out frontend list

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,7 +1,16 @@
+import { techItems } from "../../data/FrontendList";
+
 export function AboutPage() {
   return (
     <>
-     <div>about me</div>
+      <div>about me</div>
+      <div className="tech-items-container">
+        {techItems.map((tech, index) => (
+          <div key={index} className="tech-item" style={{ backgroundColor: tech.color }}>
+            {<tech.image />}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
