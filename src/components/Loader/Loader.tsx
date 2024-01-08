2a8e3aa6d72df8406cc49206b7d2488e3b8e3bce
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-08 20:52:50 +0100

    create a loader for page

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { StyledLoader} from "./loader-styled-component";
+import anime from "animejs/lib/anime.es.js";
+import { FaSpinner } from "react-icons/fa";
+
+interface Props {
+  onFinish: () => void;
+}
+
+export const Loader: React.FC<Props> = ({ onFinish }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      animateLogo();
+    }
+  }, [isMounted]);
+
+  const animateLogo = () => {
+    anime({
+      opacity: [0, 1],
+      duration: 1300,
+      easing: "linear",
+      complete: () => {
+        anime({
+          opacity: [1, 0],
+          duration: 1100,
+          easing: "linear",
+          complete: () => {
+            setTimeout(() => {
+              onFinish();
+            }, 170);
+          },
+        });
+      },
+    });
+  };
+
+  return (
+    <>
+      <StyledLoader>
+        <FaSpinner size={40} color="#fff" data-testid="fa-spinner"/>
+      </StyledLoader>
+    </>
+  );
+};
