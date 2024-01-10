fa61f66a8ca90d00af3f496881bdda701ef21025
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-10 23:19:26 +0100

    finished test for frontendlist component, checking if frontendicons are rendered

---
+++ tests/FrontendList.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { expect, it } from "vitest";
+import { techItems } from '../src/data/FrontendList';
+import { FrontendList } from '../src/pages/About/FrontendList';
+
+it('should render my frontend-list icons', () => {
+  render(<FrontendList />);
+
+  const headerElement = screen.getByText(/Frontend/i);
+  expect(headerElement).toBeInTheDocument();
+
+  techItems.forEach((_, index) => {
+    const techIcon = screen.getByTestId(`tech-icon-${index}`);
+    expect(techIcon).toBeInTheDocument();
+  });
+});
