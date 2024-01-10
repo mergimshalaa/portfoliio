81a266fa00515a8a4f4b3c96cf60c2f64bec39c2
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-10 23:12:50 +0100

    add bootstrap styling for row

--- src/pages/About/FrontendList.tsx
+++ src/pages/About/FrontendList.tsx
@@ -5,7 +5,14 @@ export function FrontendList() {
   return (
     <Container className="text-center text-icons">
       <h2>Frontend</h2>
-      <Row>
+      <Row
+        style={{
+          justifyContent: "center",
+          fontSize: "3rem",
+          color: "white",
+          gap: ".3rem",
+        }}
+      >
         {techItems.map((techItem, index) => (
           <Col xs={4} md={2} key={index}>
             <techItem.image size="50px" color={techItem.color} />
@@ -14,4 +21,4 @@ export function FrontendList() {
       </Row>
     </Container>
   );
-};
+}
