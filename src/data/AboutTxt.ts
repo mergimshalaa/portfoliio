c4aecb9b9b57d0fc2e0a0115df8380ae2252d1e2
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-12 14:25:13 +0100

    added data paragraphs to map out presentation about myself

--- src/data/AboutTxt.ts
+++ src/data/AboutTxt.ts
@@ -1,4 +1,18 @@
-interface AboutTxt {
-    description: string;
+interface Paragraphs {
+  text: string;
 }
-  
+
+export const paragraphs: Paragraphs[] = [
+  {
+    text: "My name is Mergim Shala, a passionate frontend developer who recently completed my education. With a solid foundation in web development and a keen interest in crafting user-friendly interfaces, I am committed to delivering top-notch code and inventive solutions.",
+  },
+  {
+    text: "During my studies, I focused on HTML, CSS, and JavaScript and I also gained hands-on experience with popular frontend frameworks like React and Angular. My dedication to continuous learning fuels my ambition to always strive for improvement.",
+  },
+  {
+    text: "As a former football player, I bring a team-oriented approach to my work. The values of teamwork, perseverance and hard work that I cultivated on the field have seamlessly translated into my career in technology.",
+  },
+  {
+    text: "As a frontend developer, my goal is to create websites and applications that are not only aesthetically pleasing but also provide a seamless and satisfying user experience. I am ready to take on new challenges and contribute to exciting projects.",
+  },
+];
