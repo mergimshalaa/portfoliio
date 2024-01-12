1c96b15ff7e3fc0dee4a0720f409b0dbc96bf151
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-12 14:26:12 +0100

    mapout paragraphs about me text

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,12 +1,23 @@
+import { paragraphs } from "../../data/AboutTxt";
 import { BackendList } from "./DevStack/BackendList";
 import { FrontendList } from "./DevStack/FrontendList";
+import { StyledText, TextWrapper, Wrapper } from "./aboutPage-styled-components";
+
 
 export function AboutPage() {
   return (
     <>
-      <div>about me</div>
+      <h2>Hey there!</h2>
+      <Wrapper>
+        <TextWrapper>
+          {paragraphs.map((paragraph, index) => (
+            <StyledText key={index}>{paragraph.text}</StyledText>
+          ))}
+        </TextWrapper>
+      </Wrapper>
+
       <FrontendList />
-      <BackendList /> 
+      <BackendList />
     </>
   );
 }
