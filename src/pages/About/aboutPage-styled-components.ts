7095a4bc13c1e4e16d3816ba9e74faac69576ba2
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-12 15:48:34 +0100

    add h2 and an img, tweaked styling

--- src/pages/About/aboutPage-styled-components.ts
+++ src/pages/About/aboutPage-styled-components.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const TextWrapper = styled.div`
-  flex: 1;
-  margin-right: 2rem;
+flex: 1;
+margin-right: 2rem;
 
   @media (max-width: 768px) {
     margin-right: 0;
@@ -14,7 +14,7 @@ export const TextWrapper = styled.div`
 
   @keyframes slide-in {
     0% {
-      transform: translateX(-100%);
+      transform: translateX(100%);
       opacity: 0;
     }
     100% {
@@ -34,10 +34,38 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
+  align-items: flex-start;
+  margin-top: 1rem;
 
   @media (max-width: 992px) {
     flex-direction: column;
   }
 `;
 
+export const ImgContainer = styled.div`
+  max-width: 100%;
+  text-align: center;
+  flex: 1;
+
+  img {
+    max-width: 100%;
+    height: 490px;
+  }
+
+  @media (max-width: 768px) {
+    img {
+      height: auto;
+    }
+  }
+`;
+
+export const Styledh2 = styled.h2`
+  font-size: 2rem;
+  color: white;
+  margin-top: 1rem;
+
+  @media (max-width: 520px) {
+    font-size: 1.5rem;
+    margin-left: 10px;
+  }
+`;
