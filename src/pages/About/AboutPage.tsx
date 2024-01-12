fd4e05b30eb92adb7c704a365170f470ac6080bf
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-12 15:48:49 +0100

    implement working man img in aboutpage

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,15 +1,18 @@
 import { paragraphs } from "../../data/AboutTxt";
+import ManWorkingImage from "../../img/man-office.png";
 import { BackendList } from "./DevStack/BackendList";
 import { FrontendList } from "./DevStack/FrontendList";
-import { StyledText, TextWrapper, Wrapper } from "./aboutPage-styled-components";
-
+import { ImgContainer, StyledText, TextWrapper, Wrapper, Styledh2 } from "./aboutPage-styled-components";
 
 export function AboutPage() {
   return (
     <>
-      <h2>Hey there!</h2>
       <Wrapper>
+        <ImgContainer>
+          <img height={700} src={ManWorkingImage} alt="Man working in an office" loading="lazy"/>
+        </ImgContainer>
         <TextWrapper>
+          <Styledh2>Hey there!</Styledh2>
           {paragraphs.map((paragraph, index) => (
             <StyledText key={index}>{paragraph.text}</StyledText>
           ))}
