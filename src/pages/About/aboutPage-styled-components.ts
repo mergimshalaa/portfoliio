7c540a2e1cb8a5714d1c5357d7331a5e7d36fda1
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-14 23:41:12 +0100

    remove keyframes styling in about page

--- src/pages/About/aboutPage-styled-components.ts
+++ src/pages/About/aboutPage-styled-components.ts
@@ -1,28 +1,23 @@
 import styled from 'styled-components';
 
 export const TextWrapper = styled.div`
-flex: 1;
-margin-right: 2rem;
+  flex: 1;
+  margin-right: 2rem;
 
   @media (max-width: 768px) {
     margin-right: 0;
   }
 
-  animation: slide-in 1.5s ease-out;
-  animation-fill-mode: forwards;
   opacity: 0;
+  transform: translateY(20px);
+  transition: opacity 0.5s, transform 0.5s;
 
-  @keyframes slide-in {
-    0% {
-      transform: translateX(100%);
-      opacity: 0;
-    }
-    100% {
-      transform: translateX(0);
-      opacity: 1;
-    }
+  &.visible {
+    opacity: 1;
+    transform: translateY(0);
   }
 `;
+
 
 export const StyledText = styled.p`
   color: white;
