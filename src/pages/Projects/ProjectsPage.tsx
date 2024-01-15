a268c571289e97f086a2b54f8f3dc252aea9869c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-15 18:06:01 +0100

    map out my projects (Work in progress)

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,7 +1,30 @@
+import { ProjectsData } from '../../data/ProjectsList';
+import { Container } from 'react-bootstrap';
+
+
 export function ProjectsPage() {
-    return (
-      <>
-       <div>projects</div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>
+        <h2>Projects</h2>
+        {ProjectsData.map((project, index) => (
+          <div key={index}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <Container>
+              <video width="320" height="240" controls>
+                <source src={project.videoSrc} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </Container>
+            <p>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                View Project
+              </a>
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
