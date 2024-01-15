34c97d9f89df4e88e8c4ee41ae79a12eee185b7d
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-15 21:39:32 +0100

    styling for the projects in project page

---
+++ src/pages/Projects/projects-styled-components.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ProjectCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 50px;
+  margin-bottom: 30px;
+`;
+
+export const CardDeck = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 5rem;
+  width: 100%;
+`;
+
+
+
