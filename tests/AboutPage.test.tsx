e04a895021fd8317e1b67b1611f1ec80f94b371d
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-16 22:42:54 +0100

    added the about intro text tests into abutpage test

--- tests/AboutPage.test.tsx
+++ tests/AboutPage.test.tsx
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import { it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { paragraphs } from '../src/data/AboutTxt';
 import ManWorkingImage from "../src/img/man-office.png";
 import { AboutPage } from '../src/pages/About/AboutPage';
+import Intro from "../src/pages/About/IntroTxt";
+
+describe("Intro Component", () => {
+  it('renders header with "ABOUT ME"', () => {
+    render(<Intro />);
+    const headerElements = screen.getAllByText(/ABOUT ME/i);
+    expect(headerElements.length).toBeGreaterThan(0);
+  });
+
+  it("renders a horizontal line under H1", () => {
+    render(<Intro />);
+    const horizontalLineElement = screen.getByRole("separator");
+    expect(horizontalLineElement).toBeInTheDocument();
+  });
+
+  it('renders the information paragraph', () => {
+    render(<Intro />);
+    const infoParagraph = screen.getByText(
+      /Here you will find information about me, my current skills mostly in terms of programming and technology and what I like to do in my free time./i
+    );
+    expect(infoParagraph).toBeInTheDocument();
+  });
+
+  it('renders StyledIntro component', () => {
+    render(<Intro />);
+    const styledIntroElement = screen.getByTestId('styled-intro');
+    expect(styledIntroElement).toBeInTheDocument();
+  });
+});
 
 it('renders paragraphs from AboutTxt data', () => {
   render(<AboutPage />);
