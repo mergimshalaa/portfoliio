746c2275c145086791cc719448cac8dc51c690e9
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-17 22:27:25 +0100

    mapping & styling my tool list

---
+++ src/pages/About/DevStack/ToolList.tsx
@@ -0,0 +1,28 @@
+import { Col, Container, Row } from 'react-bootstrap';
+
+import { toolItems } from '../../../data/ToolList';
+import './techlist.css';
+
+
+export function ToolsList() {
+  return (
+    <Container className="text-center text-icons">
+      <h2 className="header-tools">TOOLS</h2>
+      <Row
+        style={{
+          justifyContent: 'center',
+          paddingBottom: '50px',
+          fontSize: '3rem',
+          color: 'white',
+          gap: '.3rem',
+        }}
+      >
+        {toolItems.map((toolItem, index) => (
+          <Col xs={4} md={1} key={index} className="tech-icons border-white">
+            <toolItem.image size="50px" color={toolItem.color} data-testid={`tech-icon-${index}`} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
