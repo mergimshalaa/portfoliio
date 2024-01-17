35943ada0ec45cdf5eb1f78418b801b5380c7245
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-17 23:27:24 +0100

    format and smallfixes

--- tests/AboutPage.test.tsx
+++ tests/AboutPage.test.tsx
@@ -1,73 +1,75 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { paragraphs } from '../src/data/AboutTxt';
+import { paragraphs } from "../src/data/AboutTxt";
 import ManWorkingImage from "../src/img/man-office.png";
-import { AboutPage } from '../src/pages/About/AboutPage';
-import Intro from "../src/pages/About/IntroTxt";
+import AboutIntro from "../src/pages/About/AboutIntro";
+import { AboutPage } from "../src/pages/About/AboutPage";
 
-describe("Intro Component", () => {
+describe("About Intro Component", () => {
   it('renders header with "ABOUT ME"', () => {
-    render(<Intro />);
+    render(<AboutIntro />);
     const headerElements = screen.getAllByText(/ABOUT ME/i);
     expect(headerElements.length).toBeGreaterThan(0);
   });
 
   it("renders a horizontal line under H1", () => {
-    render(<Intro />);
+    render(<AboutIntro />);
     const horizontalLineElement = screen.getByRole("separator");
     expect(horizontalLineElement).toBeInTheDocument();
   });
 
-  it('renders the information paragraph', () => {
-    render(<Intro />);
-    const infoParagraph = screen.getByText(
-      /Here you will find information about me, my current skills mostly in terms of programming and technology and what I like to do in my free time./i
+  it('renders the "About Me" text from the text I added in the data file', () => {
+    render(<AboutIntro />);
+    const aboutMeText = screen.getByText(
+      /Here you will find information about me, my current skills mostly in terms of programming and technology and what I like to do in my freetime./i
     );
-    expect(infoParagraph).toBeInTheDocument();
+    expect(aboutMeText).toBeInTheDocument();
   });
 
-  it('renders StyledIntro component', () => {
-    render(<Intro />);
-    const styledIntroElement = screen.getByTestId('styled-intro');
+  it("renders StyledIntro component", () => {
+    render(<AboutIntro />);
+    const styledIntroElement = screen.getByTestId("styled-intro");
     expect(styledIntroElement).toBeInTheDocument();
   });
 });
 
-it('renders paragraphs from AboutTxt data', () => {
-  render(<AboutPage />);
-  paragraphs.forEach((paragraph) => {
-    const paragraphElement = screen.getByText(paragraph.text);
-    expect(paragraphElement).toBeInTheDocument();
+describe("About Intro Component", () => {
+  it("renders paragraphs from AboutTxt data", () => {
+    render(<AboutPage />);
+    paragraphs.forEach((paragraph) => {
+      const paragraphElement = screen.getByText(paragraph.text);
+      expect(paragraphElement).toBeInTheDocument();
+    });
   });
-});
 
-it('renders img with the correct attributes', () => {
-  render(<AboutPage />);
-  const imageElement = screen.getByAltText('Man working in an office');
-  expect(imageElement).toBeInTheDocument();
-  expect(imageElement).toHaveAttribute('src', ManWorkingImage);
-  expect(imageElement).toHaveAttribute('loading', 'lazy');
-  expect(imageElement).toHaveAttribute('height', '700');
-});
+  it("renders img with the correct attributes", () => {
+    render(<AboutPage />);
+    const imageElement = screen.getByAltText("Man working in an office");
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute("src", ManWorkingImage);
+    expect(imageElement).toHaveAttribute("loading", "lazy");
+    expect(imageElement).toHaveAttribute("height", "700");
+  });
 
-it('renders Styledh2 component with "Get to know me!" text', () => {
-  render(<AboutPage />);
-  const styledH2Element = screen.getByText(/Get to know me!/i);
-  expect(styledH2Element).toBeInTheDocument();
-});
+  it('renders Styledh2 component with "Get to know me!" text', () => {
+    render(<AboutPage />);
+    const styledH2Element = screen.getByText(/Get to know me!/i);
+    expect(styledH2Element).toBeInTheDocument();
+  });
 
-it('applies "visible" class to TextWrapper after scrolling', () => {
-  render(<AboutPage />);
-  const textWrapperElement = screen.getByTestId('text-wrapper');
-  
-  window.scrollY = 500;
-  window.dispatchEvent(new Event('scroll'));
+  it('applies "visible" class to TextWrapper after scrolling', () => {
+    render(<AboutPage />);
+    const textWrapperElement = screen.getByTestId("text-wrapper");
 
-  expect(textWrapperElement).toHaveClass('visible');
-});
+    window.scrollY = 500;
+    window.dispatchEvent(new Event("scroll"));
 
-it('applies "hidden" class to TextWrapper initially', () => {
-  render(<AboutPage />);
-  const textWrapperElement = screen.getByTestId('text-wrapper');
-  expect(textWrapperElement).toHaveClass('hidden');
+    expect(textWrapperElement).toHaveClass("visible");
+  });
+
+  it('applies "hidden" class to TextWrapper initially', () => {
+    render(<AboutPage />);
+    const textWrapperElement = screen.getByTestId("text-wrapper");
+    expect(textWrapperElement).toHaveClass("hidden");
+  });
 });
