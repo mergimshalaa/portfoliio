24e70d61dfab56436cd37609bcb95b470b2419f4
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-17 23:28:05 +0100

    reused test for projects test for intro & formatted with prettier

--- tests/ProjectsPage.test.tsx
+++ tests/ProjectsPage.test.tsx
@@ -1,59 +1,92 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { expect, it } from "vitest";
+import { expect, it, describe } from "vitest";
 import { StyledVideoCard } from "../src/pages/Projects/ProjectCards/project-card-styled-components";
 import { ProjectsPage } from "../src/pages/Projects/ProjectsPage";
+import ProjectIntro from "../src/pages/Projects/ProjectIntro";
 
-it("renders VideoCard component with expected elements", () => {
-  const mockVideoCardProps = {
-    title: "Test Title",
-    description: "Test Description",
-    videoSrc: "test-video.mp4",
-    link: "/test-link",
-  };
+describe("Project intro Component", () => {
+  it('renders header with "PROJECTS"', () => {
+    render(<ProjectIntro />);
+    const headerElements = screen.getAllByText(/PROJECTS/i);
+    expect(headerElements.length).toBeGreaterThan(0);
+  });
 
-  render(
-    <MemoryRouter>
-      <StyledVideoCard {...mockVideoCardProps} />
-    </MemoryRouter>
-  );
+  it("renders a horizontal line under H2", () => {
+    render(<ProjectIntro />);
+    const horizontalLineElement = screen.getByRole("separator");
+    expect(horizontalLineElement).toBeInTheDocument();
+  });
 
-  expect(screen.getByText(mockVideoCardProps.title)).toBeInTheDocument();
-  expect(screen.getByText(mockVideoCardProps.description)).toBeInTheDocument();
-  expect(
-    screen.getByRole("button", { name: /click here/i })
-  ).toBeInTheDocument();
+  it('renders the "Projects" text from the text I added in the data file', () => {
+    render(<ProjectIntro />);
+    const projectText = screen.getByText(
+      /Here you will find a list of projects I have worked on, both personal and during my schooltime./i
+    );
+    expect(projectText).toBeInTheDocument();
+  });
+
+  it("renders StyledIntro component", () => {
+    render(<ProjectIntro />);
+    const styledIntroElement = screen.getByTestId("styled-intro");
+    expect(styledIntroElement).toBeInTheDocument();
+  });
 });
 
-it("renders ProjectsPage component with heading", () => {
-  render(
-    <MemoryRouter>
-      <ProjectsPage />
-    </MemoryRouter>
-  );
+describe("Projects Component", () => {
+  it("renders VideoCard component with expected elements", () => {
+    const mockVideoCardProps = {
+      title: "Test Title",
+      description: "Test Description",
+      videoSrc: "test-video.mp4",
+      link: "/test-link",
+    };
 
-  expect(
-    screen.getByRole("heading", { name: /projects/i })
-  ).toBeInTheDocument();
-});
+    render(
+      <MemoryRouter>
+        <StyledVideoCard {...mockVideoCardProps} />
+      </MemoryRouter>
+    );
 
-it("renders at least one project title in ProjectsPage component", () => {
-  render(
-    <MemoryRouter>
-      <ProjectsPage />
-    </MemoryRouter>
-  );
+    expect(screen.getByText(mockVideoCardProps.title)).toBeInTheDocument();
+    expect(
+      screen.getByText(mockVideoCardProps.description)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /click here/i })
+    ).toBeInTheDocument();
+  });
 
-  const mockProjectsData = [
-    {
-      title: "Filmflix",
-      description:
-        "Group project where we created a 'Netflix', focus was writing tests for the application.",
-    },
-  ];
+  it("renders ProjectsPage component with heading", () => {
+    render(
+      <MemoryRouter>
+        <ProjectsPage />
+      </MemoryRouter>
+    );
 
-  mockProjectsData.forEach((project) => {
-    expect(screen.getByText(project.title)).toBeInTheDocument();
-    expect(screen.getByText(project.description)).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /projects/i })
+    ).toBeInTheDocument();
+  });
+
+  it("renders at least one project title in ProjectsPage component", () => {
+    render(
+      <MemoryRouter>
+        <ProjectsPage />
+      </MemoryRouter>
+    );
+
+    const mockProjectsData = [
+      {
+        title: "Filmflix",
+        description:
+          "Group project where we created a 'Netflix', focus was writing tests for the application.",
+      },
+    ];
+
+    mockProjectsData.forEach((project) => {
+      expect(screen.getByText(project.title)).toBeInTheDocument();
+      expect(screen.getByText(project.description)).toBeInTheDocument();
+    });
   });
 });
