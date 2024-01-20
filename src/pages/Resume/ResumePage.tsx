559caa7bc6f25da910529d595bd1adb5ba78e125
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-20 03:25:25 +0100

    resumepage done, add pdf to download logic

--- src/pages/Resume/ResumePage.tsx
+++ src/pages/Resume/ResumePage.tsx
@@ -1,7 +1,28 @@
+import PageOne from '../Resume/cvimg/Page1.jpeg'
+import PageTwo from '../Resume/cvimg/Page2.jpeg'
+import resumePDF from '../Resume/mergimshala.pdf';
+import { GridContainer, StyledButton, StyledImage, Styledh2, Container } from './resume-styled-component';
+
+function downloadResume() {
+  const link = document.createElement('a');
+  link.href = resumePDF;
+  link.download = 'mergimshalaCV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 export function ResumePage() {
-    return (
-      <>
-       <div>resume</div>
-      </>
-    );
-  }
+  return (
+    <>
+      <Styledh2>RESUME</Styledh2>
+      <GridContainer>
+        <StyledImage src={PageOne} alt="Page One" />
+        <StyledImage src={PageTwo} alt="Page Two" />
+      </GridContainer>
+      <Container>
+        <StyledButton onClick={downloadResume}>DOWNLOAD CV</StyledButton>
+      </Container>
+    </>
+  );
+}
