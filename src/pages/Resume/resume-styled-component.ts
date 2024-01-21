a59c5f3b5eb1e61e7571385bbab4cc3ffecc2bc2
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-21 21:34:08 +0100

    add mediaqueries for resume page, fully responsive now

--- src/pages/Resume/resume-styled-component.ts
+++ src/pages/Resume/resume-styled-component.ts
@@ -6,6 +6,16 @@ export const Styledh2 = styled.h2`
   color: #fff;
   border-radius: 4px;
   letter-spacing: 2px;
+
+  @media (max-width: 768px) {
+    font-size: 28px;
+    padding: 10px 16px;
+  }
+
+  @media (max-width: 380px) {
+    font-size: 20px;
+    padding: 6px 12px;
+  }
 `;
 
 export const GridContainer = styled.div`
@@ -14,6 +24,11 @@ export const GridContainer = styled.div`
   justify-content: center;
   align-items: center;
   gap: 1rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    margin-right: 1rem;
+  }
 `;
 
 export const StyledImage = styled.img`
@@ -41,9 +56,27 @@ export const StyledButton = styled.button`
   padding: 1.2rem 2rem;
   border-radius: 2rem;
   text-decoration: none;
+  text-align: center;
+  border: 2px solid #2196f3;
+  transition: all 0.2s ease-in-out;
 
   &:hover {
     background-color: #2196f3;
     border-color: #2196f3;
+  }
+  
+  &:focus {
+    outline: none;
+  }
+  
+  &:active {
+    transform: scale(0.98);
+  }
+
+  @media (max-width: 768px) {
+    padding: 14px 1rem;
+    font-size: 16px;
+    margin-top: 2.3rem;
+    margin-bottom: 2.3rem;
   }
 `;
