b007cf9b1f86dd7d533ca722b58b6b6bb5d18731
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2024-01-24 15:20:57 +0100

    tweak

--- src/data/IntroTxt.ts
+++ src/data/IntroTxt.ts
@@ -6,7 +6,7 @@ interface IntroTxt {
 export const introTxt: IntroTxt[] = [
   {
     AboutPageText:
-    "Here you will find information about me, my current skills mostly in terms of programming and technology and what I like to do in my freetime.",
+    "Here you will find information about me, my current skills mostly in terms of programming and technology.",
     ProjectPageText:
     "Here you will find a list of projects I have worked on, both personal and during my schooltime."
   },
